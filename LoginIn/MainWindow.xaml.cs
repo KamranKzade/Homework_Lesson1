1a285ebf762d98edd90b7cf6228b9d856a8d8e08
Kamran <311.kamran@gmail.com>
2022-11-11 15:23:09 +0400

    Complete Master LogIn

--- LoginIn/MainWindow.xaml.cs
+++ LoginIn/MainWindow.xaml.cs
@@ -1,22 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using LogIn_Master.Models;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace LogIn_Master;
 
 public partial class MainWindow : Window
 {
+    public Student student = new("Kamran", "Kamran123");
+
     public MainWindow()
     {
         InitializeComponent();
@@ -32,4 +23,26 @@ public partial class MainWindow : Window
     private void Minimize_Btn(object sender, RoutedEventArgs e) => WindowState = WindowState.Minimized;
     private void Close_Btn(object sender, RoutedEventArgs e) => Application.Current.Shutdown();
 
+    private void Login_Click(object sender, RoutedEventArgs e)
+    {
+        if (student.Username == Username.Text)
+        {
+            textblock.Text = string.Empty;
+            if (student.Password == Password.Password)
+            {
+                MessageBox.Show($"Welcome {student.Username}", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+                textblock.Text = string.Empty;
+            }
+            else
+            {
+                textblock.Text = "Your Password is Wrong";
+                textblock.Visibility = Visibility.Visible;
+            }
+        }
+        else
+        {
+            textblock.Text = "Your Username is Wrong";
+            textblock.Visibility = Visibility.Visible;
+        }
+    }
 }
