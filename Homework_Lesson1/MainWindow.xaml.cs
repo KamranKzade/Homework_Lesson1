458268814448365bbeb551ae59c4ad1144a38dda
Kamran <311.kamran@gmail.com>
2022-11-11 07:30:12 +0400

    Add project files.

---
+++ Homework_Lesson1/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace Homework_Lesson1;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+
+
+    private void Click(object sender, MouseButtonEventArgs e)
+    {
+        var btn = sender as Button;
+
+        byte a = (byte)Random.Shared.Next(0, 256);
+        byte b = (byte)Random.Shared.Next(0, 256);
+        byte c = (byte)Random.Shared.Next(0, 256);
+
+        if (e.RightButton == MouseButtonState.Pressed)
+        {
+            Title = btn?.Content.ToString();
+
+            foreach (var item in MyStackPanel.Children)
+            {
+                if (item is StackPanel stack)
+                {
+                    stack.Children.Remove(btn);
+                }
+            }
+        }
+        else if (e.LeftButton == MouseButtonState.Pressed)
+        {
+            btn.Background = new SolidColorBrush(Color.FromRgb(a, b, c));
+            MessageBox.Show($"Color: {btn.Background}", "Informaton", MessageBoxButton.OK, MessageBoxImage.Information);
+
+        }
+    }
+}
